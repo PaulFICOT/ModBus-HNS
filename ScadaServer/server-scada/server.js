30a65b6d21e24cd5a4b9426fc27e43499a9be284
Paul FICOT <paul.ficot@gmail.com>
2021-01-26 13:51:10 +0100

    href update py js & ejs updates

--- ScadaServer/server-scada/server.js
+++ ScadaServer/server-scada/server.js
@@ -1,6 +1,6 @@
 // load the things we need
-var express = require('express');
-var app = express();
+let express = require('express');
+let app = express();
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
@@ -10,20 +10,24 @@ app.use(express.static(__dirname + '/static'));
 
 // index page 
 app.get('/', function(req, res) {
-    var valeurs = [
-        { name: 'Ma variable1', comment: "Projet", address: "%MW0", value: 18},
-        { name: 'Ma variable2', comment: "Projet", address: "%MW1", value: 19},
-        { name: 'Ma variable3', comment: "Projet", address: "%MW2", value: 20},
-        { name: 'Ma variable4', comment: "Projet", address: "%MW3", value: 21}
+    let plc_name;
+    plc_name = "PLC0";
+
+    let valeurs = [
+        {name: 'Ma variable1', comment: "Projet", address: "%MW0", value: 18},
+        {name: 'Ma variable2', comment: "Projet", address: "%MW1", value: 19},
+        {name: 'Ma variable3', comment: "Projet", address: "%MW2", value: 20},
+        {name: 'Ma variable4', comment: "Projet", address: "%MW3", value: 21}
     ];
 
-    var tagline = "Ceci est un test d'une page avec ejs";
+    let tagline = "Ceci est un test d'une page avec ejs";
 
     res.render('pages/index', {
+        plc_name: plc_name,
         valeurs: valeurs,
         tagline: tagline
     });
 });
 
 app.listen(8080);
-console.log('Serveur web port 8080...');
+console.log('Server web port 8080...');
