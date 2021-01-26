b801c25d6a3ac7b598e5373840cad0e3c32afcc8
Nathan <fourrier.nathan@outlook.fr>
2021-01-26 15:34:30 +0100

    Doing the web interface for PLC and a bit of refactoring

---
+++ PLC/server-scada/server.js
@@ -0,0 +1,59 @@
+//Imports
+
+const express = require('express');
+const app = express();
+const bodyParser = require("body-parser");
+
+// set the app
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/static'));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+
+// Conf vars
+const webServPort = 8080;
+const plc_name = "PLC_0";
+
+var variables = [{
+  name: 'Ma variable1',
+  comment: "Projet",
+  address: "%MW0",
+  value: 18
+}];
+
+
+// Home page get
+app.get('/', function(req, res) {
+
+  res.render('pages/index', {
+    plc_name: plc_name,
+    valeurs: variables
+  });
+});
+
+// post /add route
+app.post("/add", function(req, res) {
+  const variable = {
+    name: req.body.name,
+    comment: req.body.comment,
+    address: req.body.address,
+    value: req.body.value
+  };
+  variables.push(variable);
+  res.redirect("/");
+});
+
+
+// post /update route
+app.post("/update", function(req, res) {
+  //TODO: make update post function
+  console.log(req.body);
+
+  res.redirect("/");
+});
+
+
+app.listen(webServPort);
+console.log("Server web port " + webServPort + "...");
---
+++ PLC/server-scada/static/index.js
@@ -0,0 +1,9 @@
+$(".delete-button").click(function() {
+ var id = $(this).attr("id");
+ $(".delete-button").click(function() {
+ var id = $(this).attr("id");
+alert(id);
+
+});alert(id);
+
+});
