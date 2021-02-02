e82283b24949c35a7940ca9bc3980c6a1226eb6b
nathan <fourrier.nathan@outlook.fr>
2021-02-02 21:35:59 +0100

    a bit of refactoring

--- PLC/modbusServer.js
+++ PLC/modbusServer.js
@@ -1,38 +1,23 @@
-'use strict'
+'use strict';
+// Imports
+const net = require('net');
+const modbus = require('jsmodbus');
 
-const net = require('net')
-const modbus = require('jsmodbus')
-const netServer = new net.Server()
-const holding = Buffer.alloc(10000)
-const server = new modbus.server.TCP(netServer, {
-  holding: holding
-})
+// Init
+const netServer = new net.Server();
+const server = new modbus.server.TCP(netServer);
 
-
-
+// Log connections to server
 server.on('connection', function (client) {
-  console.log('New Connection')
-})
+  console.log('New Connection');
+});
 
+// Listener to log request or do specific actions when a request is detected
+// server.on('WriteSingleRegister', function (value, address) {})
+// server.on('writeMultipleCoils', function (value) {})
+// server.on('writeMultipleRegisters', function (value) {})
 
-server.on('WriteSingleRegister', function (value, address) {
-  //TODO: Update the DB with server.holding.readUInt16BE(address)
-  
-})
-
-server.on('writeMultipleCoils', function (value) {
-  //Update DB
-  console.log('Write multiple coils - Existing: ', value)
-})
-
-
-server.on('writeMultipleRegisters', function (value) {
-  //Update DB
-
-  // webServer.updatewordValue(address,value);
-})
+// Port define in parameter or default is 8502
 const modbusPort = process.argv[2] || 8502;
-console.log("Modbus server started on port " + modbusPort+"...")
-netServer.listen(modbusPort)
-
-exports.server = server;
+console.log('Modbus server started on port ' + modbusPort + '...');
+netServer.listen(modbusPort);
